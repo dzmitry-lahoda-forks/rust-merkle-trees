8f05e05d005789adf7c643da51d46932cffc1b54
Dzmitry Lahoda <dzmitry@lahoda.pro>
2022-08-06 21:25:44 +0100

    nix starter

--- flake.nix
+++ flake.nix
@@ -29,61 +29,24 @@
         craneLib = crane.lib.${system};
         src = ./.;
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
         cargoArtifacts = craneLib.buildDepsOnly {
           inherit src;
         };
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
         rust-merkle-trees = craneLib.buildPackage {
           inherit cargoArtifacts src;
+          doCheck = false;
         };
       in
       {
         checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit rust-merkle-trees;
-
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, resuing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          my-crate-clippy = craneLib.cargoClippy {
-            inherit cargoArtifacts src;
-            cargoClippyExtraArgs = "-- --deny warnings";
-          };
-
-          # Check formatting
-          my-crate-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-
-          # Audit dependencies
-          my-crate-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `my-crate` if you do not want
-          # the tests to run twice
-          my-crate-nextest = craneLib.cargoNextest {
+          rust-merkle-trees-tests = craneLib.cargoNextest {
             inherit cargoArtifacts src;
             partitions = 1;
             partitionType = "count";
           };
-        } // lib.optionalAttrs (system == "x86_64-linux") {
-          # NB: cargo-tarpaulin only supports x86_64 systems
-          # Check code coverage (note: this will not upload coverage anywhere)
-          my-crate-coverage = craneLib.cargoTarpaulin {
-            inherit cargoArtifacts src;
-          };
-        };
+        };        
 
-        packages.default = my-crate;
+        packages.default = rust-merkle-trees;
       });
 }
